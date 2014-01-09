f7f8363154e5a4a6359fe98d7db91ce77e20fa2d
unknown <thomasl@COLOMBO.GISTEK.LOCAL>
2014-01-09 13:41:39 +0100

    added responsive design

---
+++ js/CustomAgol.js
@@ -0,0 +1,9 @@
+function toggleMapTaskList()
+{
+	var elem = document.getElementById("taskListDiv");
+	if(elem.style.display == 'block')
+		elem.style.display = 'none';
+	else
+		elem.style.display = 'block';
+
+}
